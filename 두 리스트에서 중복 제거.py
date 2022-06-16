9025114d81e5849c01faad82b979c581e1881387
morethanmini <dltkd2786@gmail.com>
2022-06-16 17:36:51 +0900

    2022.06.16

--- 두 리스트에서 중복 제거.py
+++ 두 리스트에서 중복 제거.py
@@ -2,11 +2,11 @@ n = [1, 2, 3, 4, 5]
 lost = [2, 4]
 result = []
 
-for i in n:
+for i in n: # 방법 1
     if i not in lost:
         result.append(i)
 
-result1 = [i for i in n if i not in lost]
+result1 = [i for i in n if i not in lost] # 방법 2
 
 print(result)
 print(result1)
